fc96ec1b8fde2c0bcf9b201130b7bc6d69e4a20d
txxm <2350094959@qq.com>
2019-02-28 00:16:22 +0800

    bubble.go

---
+++ sort/bubble.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func bubble(array []int, l int, r int) {
+	if array == nil {
+		return
+	}
+
+	for i := 0; i < r-1; i++ {
+		for j := 0; j < r-i; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
+
+	fmt.Println(array)
+}
+
+func main() {
+	array := []int {3, 5, 1, 7, 0, 6, 4, 9, 8, 2}
+
+	bubble(array, 0, 9)
+}
